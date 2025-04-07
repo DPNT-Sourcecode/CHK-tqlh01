e6894b50ca6c6939faf8840d6135fa78c3135118
sbx_user1051 <sbx_user1051@169.254.9.5>
2025-04-07 21:17:38 +0000

    Mon Apr 07 21:17:38 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,13 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
-        # offers = {"A": [(5, 200), (3, 130)], "B": [(2, 45)], "C": [None], "D": [None], "E": [None]}
+        prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20,
+                  "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40,
+                  "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40,
+                "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
+        
 
-        count = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0}
+        count = {item : 0 for item in prices.keys()}
 
         for sku in skus:
             if sku in count:

9191405d3212ebb76771906f37cb8f267c5adc46
sbx_user1051 <sbx_user1051@169.254.9.5>
2025-04-07 19:39:59 +0000

    Mon Apr 07 19:39:59 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,22 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-        offers = {"A": (3, 130), "B": (2, 45), "C": None, "D": None}
+        prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+        offers = {"A": [(3, 130), (5, 200)], "B": [(2, 45)], "C": [None], "D": [None], "E": [(2, 0, "B")]}
 
         count = {"A": 0, "B": 0, "C": 0, "D": 0}
+
+        free_b_count = 0
+        count_e = 0
 
         for sku in skus:
             if sku in count:
                 count[sku] += 1
+                if sku == "E":
+                    count_e += 1
+                    if count_e == 2:
+                        free_b_count += 1
+                        count_e = 0
             else:
                 return -1
             

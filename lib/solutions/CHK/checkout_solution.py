57c3d7fc557f1fd990df84126bd3274cf8e87eab
sbx_user1051 <sbx_user1051@169.254.9.5>
2025-04-07 19:51:59 +0000

    Mon Apr 07 19:51:59 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,34 +8,16 @@ class CheckoutSolution:
 
         count = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
 
-        free_b_count = 0
-        count_e = 0
-
         for sku in skus:
             if sku in count:
                 count[sku] += 1
-                if sku == "E":
-                    count_e += 1
-                    if count_e == 2:
-                        free_b_count += 1
-                        count_e = 0
             else:
                 return -1
-            
+        
+        free_b_count = count["E"] // 2
+        remaining_b = count["B"] - free_b_count
         total_price = 0
 
-        for item, item_count in count.items():
-            if offers[item] is None:
-                total_price += item_count * prices[item]
-            else:
-                offers_list = offers[item]
-                if len(offers_list) >= 1:
-                    for offer in offers_list:
-                        offer_count, offer_price = offer[item]
-                        
-                        num_offers = item_count // offer_count
-                        remaining = item_count % offer_count
-
-                        total_price += offer_price * num_offers + remaining * prices[item]
+        
 
         return total_price

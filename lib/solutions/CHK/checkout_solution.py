e7c4363cfb223fb8990771b07871cc71f1b24141
sbx_user1051 <sbx_user1051@169.254.9.5>
2025-04-07 21:43:06 +0000

    Mon Apr 07 21:43:06 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,8 +5,8 @@ class CheckoutSolution:
     def checkout(self, skus):
         prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20,
                   "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40,
-                  "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40,
-                "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
+                  "O": 10, "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20, "U": 40,
+                "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
         
 
         count = {item : 0 for item in prices.keys()}
@@ -120,6 +120,7 @@ class CheckoutSolution:
                 total_price += value * prices[sku]
 
         return total_price
+
 
 
 

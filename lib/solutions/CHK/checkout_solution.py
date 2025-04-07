90519fc8e33ca2cc96c8bb8e708f8eaae1b3a099
sbx_user1051 <sbx_user1051@169.254.9.5>
2025-04-07 21:46:03 +0000

    Mon Apr 07 21:46:03 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,9 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20,
-                  "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40,
+                  "H": 10, "I": 35, "J": 60, "K": 70, "L": 90, "M": 15, "N": 40,
                   "O": 10, "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20, "U": 40,
-                "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
+                  "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
         
 
         count = {item : 0 for item in prices.keys()}
@@ -84,7 +84,7 @@ class CheckoutSolution:
                 total_price += value * prices["H"]
             elif sku == "K":
                 k_offers = value // 2
-                total_price += k_offers * 150
+                total_price += k_offers * 120
                 value -= k_offers * 2
 
                 total_price += value * prices["K"]
@@ -120,6 +120,7 @@ class CheckoutSolution:
                 total_price += value * prices[sku]
 
         return total_price
+
 
 
 
